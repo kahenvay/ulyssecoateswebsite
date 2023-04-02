7ad6fae10e83dba51c78c7d1ed7e711029cfee50
Ulysse Coates <u.coates@gmail.com>
2023-04-02 19:35:38 +0100

    testing contentfull, putting a comming soon simple text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,30 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
-  siteMetadata: {
-    title: `ulyssecoates.com`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
+	siteMetadata: {
+		title: `Ulyssecoates.com`,
+		siteUrl: `https://ulyssecoates.com`,
+		description: `I'm Ulysse Coates, and I like making Websites!`,
+	},
+	plugins: [
+		`gatsby-plugin-image`,
+		`gatsby-plugin-sharp`,
+		`gatsby-transformer-sharp`,
+		{
+			resolve: `gatsby-source-contentful`,
+			options: {
+				spaceId: process.env.SPACE_ID,
+				// Learn about environment variables: https://gatsby.dev/env-vars
+				accessToken: process.env.CONTENT_DELIVERY_KEY,
+				// accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+				// host: `preview.contentful.com`,
+			},
+		},
+	],
+};
